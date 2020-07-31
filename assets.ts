2d55854b909a29da2c41e2e13becf10795b497e0
shakao <34112083+shakao@users.noreply.github.com>
2020-07-31 15:28:29 -0700

    Initial files for MakeCode project

---
+++ assets.ts
@@ -0,0 +1,17 @@
+namespace rpgitemsSprites {
+    //% fixedInstance jres blockIdentity=images._image
+    export const potionred = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const potiongreen = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const potionyellow = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const sword = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const sword2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const bone = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const skull = image.ofBuffer(hex``);
+
+}
